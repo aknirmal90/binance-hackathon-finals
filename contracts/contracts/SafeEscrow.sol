21d44de1dc4c438e3b6bd9b6a205653cda430892
astudnev <astudnev@gmail.com>
2019-01-19 17:06:28 +0800

    events added

--- contracts/contracts/SafeEscrow.sol
+++ contracts/contracts/SafeEscrow.sol
@@ -9,7 +9,7 @@ contract SafeEscrow  is Ownable {
 
     struct Lender {
         uint deposited_amount;
-        uint available_amount;
+        uint locked_amount;
     }
 
     struct Profile {
@@ -21,24 +21,52 @@ contract SafeEscrow  is Ownable {
     mapping( address => Lender)  lenders;
     Profile[] profiles;
 
+
+    event Deposit(address indexed lender, uint amount, uint total_deposited_amount);
+    event Withdraw(address indexed lender, uint amount, uint total_deposited_amount);
+    event NewProfile(address indexed lender, uint max_risc_score, uint premium_percentage);
+
+
+    // lender deposit amount using fallback payable
     function() external payable {
+        lenders[msg.sender].deposited_amount = lenders[msg.sender].deposited_amount.add(msg.value);
+
+        emit Deposit(msg.sender, msg.value, lenders[msg.sender].deposited_amount);
+    }
+
+    // specify the condition of the lending
+    function addProfile( uint max_risc_score, uint premium_percentage) external {
+        require(lenders[msg.sender].deposited_amount>0);
+
+        profiles.push(Profile(msg.sender,max_risc_score,premium_percentage));
+
+        emit NewProfile(msg.sender, max_risc_score, premium_percentage);
 
     }
 
+    // lender can withdraw funds, not including locked amount
+    function withdraw(uint amount) external {
+        Lender storage lender = lenders[msg.sender];
+
+        require(lender.deposited_amount>=lender.locked_amount.add(amount));
+        require(amount>0);
+
+        lender.deposited_amount = lender.deposited_amount.sub(amount);
+
+        msg.sender.transfer(amount);
+
+        emit Withdraw(msg.sender, amount, lenders[msg.sender].deposited_amount);
+    }
+
+    /// get lender information
     function getLenderInfo(uint index) public view returns(address, uint, uint, uint, uint) {
         Profile storage profile = profiles[index];
         Lender storage lender = lenders[profile.lender_address];
         return (    profile.lender_address,
                     lender.deposited_amount,
-                    lender.available_amount,
+                    lender.deposited_amount.sub(lender.locked_amount),
                     profile.max_risc_score,
                     profile.premium_percentage);
     }
 
-
-
-    //function addMeAsLender( uint max_risc_score, uint premium_percentage) public payable {
-    //    lender_addresses.push(msg.sender);
-    //    lenders[msg.sender] = Lender(msg.value, 0, max_risc_score,premium_percentage);
-    //}
 }
