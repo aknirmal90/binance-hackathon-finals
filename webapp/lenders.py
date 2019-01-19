f85b313308b905c824f270105ae227fb2b06d082
astudnev <astudnev@gmail.com>
2019-01-19 14:11:13 +0800

    lenders API

---
+++ webapp/lenders.py
@@ -0,0 +1,62 @@
+from web3 import Web3
+import json
+
+abi = '''[
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "index",
+				"type": "uint256"
+			}
+		],
+		"name": "getLenderInfo",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			},
+			{
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]'''
+
+ABI = json.loads(abi)
+
+def lenders():
+    w3 = Web3(Web3.HTTPProvider("https://ropsten.infura.io/v3/41d6ff4c88864dd996974369a8f27c81"))
+    contract = w3.eth.contract(address='0x42D2B6398244a93a8c95B4942d184dA8bEe88a83', abi=ABI)
+    i = 0
+    lenders = []
+    while i < 10:
+        try:
+            lender_info = contract.functions.getLenderInfo(i).call()
+            lenders.append({
+                'address': lender_info[0],
+                'deposited_amount': lender_info[1],
+                'available_amount': lender_info[2],
+                'max_risc_score': lender_info[3],
+                'premium_percentage': lender_info[4]
+            })
+        except:
+            break
+        i+=1
+    return lenders
