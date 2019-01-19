edbcb3d0e8f745d8022dc96039ce7466808169fe
Nirmal AK <aknirmal90@gmail.com>
2019-01-19 19:24:39 +0800

    2 charts drawn

---
+++ lenders.py
@@ -0,0 +1,82 @@
+from web3 import Web3
+import json
+
+abi = '''[
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "index",
+				"type": "uint256"
+			}
+		],
+		"name": "getLenderInfo",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			},
+			{
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]'''
+
+ABI = json.loads(abi)
+
+def get_lenders():
+	w3 = Web3(Web3.HTTPProvider("https://ropsten.infura.io/v3/41d6ff4c88864dd996974369a8f27c81"))
+	escrow_address='0x42D2B6398244a93a8c95B4942d184dA8bEe88a83'
+	contract = w3.eth.contract(address=escrow_address, abi=ABI)
+
+	i = 0
+	lenders = []
+	while i < 10:
+		try:
+			lender_info = contract.functions.getLenderInfo(i).call()
+			lenders.append({
+				'address': lender_info[0],
+				'deposited_amount': lender_info[1],
+				'available_amount': lender_info[2],
+				'max_risc_score': lender_info[3],
+				'premium_percentage': lender_info[4]
+			})
+		except:
+			break
+		i+=1
+
+	lender_profile = {}
+	for lender in lenders:
+		max_lender_risk_score = lender.get('max_risc_score')
+		max_premium_for_risk = lender_profile.get(max_lender_risk_score, 100)
+
+		if max_premium_for_risk > lender['premium_percentage']:
+			max_premium_for_risk = lender['premium_percentage']
+		lender_profile[max_lender_risk_score] = max_premium_for_risk
+	lender_profile_sorted = [(key, value) for key, value in lender_profile.items()]
+	lender_profile_sorted.sort(key=lambda x: x[0])
+
+	continous_profile = []
+	prev_lender_risk = 0
+
+	for lender_risk, lender_premium in lender_profile_sorted:
+		continous_profile.extend([(str(i+1), lender_premium) for i in range(prev_lender_risk, lender_risk)])
+		prev_lender_risk = lender_risk
+	return continous_profile
--- views.py
+++ views.py
@@ -5,6 +5,12 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from services.models import get_address_risk_metrics
 from django.shortcuts import redirect
+from lenders import get_lenders
+from bokeh.embed import components
+from bokeh.plotting import figure
+from bokeh.models.glyphs import Step
+from bokeh.models import ColumnDataSource, Plot, LinearAxis
+from bokeh.models.annotations import Title
 
 
 class QueryFormView(TemplateView):
@@ -25,14 +31,46 @@ class QueryFormView(TemplateView):
 		risk_metrics = get_address_risk_metrics(address)
 		risk_score = risk_metrics['risk_score']
 		risk_features = risk_metrics['risk_features']
+		lenders_features = get_lenders()
+		script, div = self._components(lenders_features)
 
 		context = {
 			'form': form, 
 			'risk_score': risk_score, 
 			'risk_features': risk_features,
+			'script': script,
+			'div': div,
 			'destination_address': address
 		}
 		return render(request, self.template_name, context)
+
+	def _components(self, lenders_features):		
+		x = [feature[0] for feature in lenders_features]
+		y = [feature[1] for feature in lenders_features]
+
+		source = ColumnDataSource(dict(x=x, y=y))
+		t = Title()
+		t.text = 'Risk Profile for Lenders'
+
+		plot = Plot(
+		    title=t, plot_width=500, plot_height=300,
+		    h_symmetry=False, v_symmetry=False, min_border=0, toolbar_location=None)
+
+		glyph1 = Step(x="x", y="y", line_color="#f46d43", mode="before")
+		plot.add_glyph(source, glyph1)		
+		plot.xaxis.ticker = x
+		plot.yaxis.ticker = y
+
+		xaxis = LinearAxis(axis_label="Risk Score")
+		plot.add_layout(xaxis, 'below')
+
+		yaxis = LinearAxis(axis_label="% Premium Payable")
+		plot.add_layout(yaxis, 'left')
+
+		plot.xgrid.grid_line_color = None
+		plot.y_range.start = 0
+		script, div = components(plot)
+		return script, div
 
 
 def api_view(request):
