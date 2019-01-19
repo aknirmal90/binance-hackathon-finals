95583f6eeffc268b40c942acc7f5112c52c1ef9a
astudnev <astudnev@gmail.com>
2019-01-19 18:34:15 +0800

    oraclize added

--- contracts/contracts/SafeEscrow.sol
+++ contracts/contracts/SafeEscrow.sol
@@ -1,9 +1,12 @@
-pragma solidity ^0.5.0;
+pragma solidity 0.4.25;
 
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract SafeEscrow  is Ownable {
+import "github.com/oraclize/ethereum-api/oraclizeAPI_0.4.sol";
+
+
+contract SafeEscrow  is Ownable, usingOraclize {
 
     using SafeMath for uint256;
 
@@ -14,17 +17,36 @@ contract SafeEscrow  is Ownable {
 
     struct Profile {
         address lender_address;
-        uint    max_risc_score;
+        uint    max_risk_score;
         uint    premium_percentage;
     }
 
-    mapping( address => Lender)  lenders;
-    Profile[] profiles;
+    struct Insurance {
 
+        address sender;
+        address destination;
+
+        uint    amount;
+
+        uint    risk_score;
+        uint    profile_index;
+
+        uint    premium_fee;
+
+    }
+
+    mapping( address => Lender)  lenders;
+
+    Profile[]       profiles;
+    Insurance[]     insurances;
+
+    mapping ( bytes32 => Insurance) quotes;
 
     event Deposit(address indexed lender, uint amount, uint total_deposited_amount);
     event Withdraw(address indexed lender, uint amount, uint total_deposited_amount);
-    event NewProfile(address indexed lender, uint max_risc_score, uint premium_percentage);
+    event NewProfile(address indexed lender, uint max_risk_score, uint premium_percentage);
+    event NewQuote(address indexed sender, address indexed receiver, uint amount);
+    event DoneQuote(address indexed sender, address indexed receiver, uint amount, uint risk_score);
 
 
     // lender deposit amount using fallback payable
@@ -35,12 +57,12 @@ contract SafeEscrow  is Ownable {
     }
 
     // specify the condition of the lending
-    function addProfile( uint max_risc_score, uint premium_percentage) external {
+    function addProfile( uint max_risk_score, uint premium_percentage) external {
         require(lenders[msg.sender].deposited_amount>0);
 
-        profiles.push(Profile(msg.sender,max_risc_score,premium_percentage));
+        profiles.push(Profile(msg.sender,max_risk_score,premium_percentage));
 
-        emit NewProfile(msg.sender, max_risc_score, premium_percentage);
+        emit NewProfile(msg.sender, max_risk_score, premium_percentage);
 
     }
 
@@ -65,8 +87,56 @@ contract SafeEscrow  is Ownable {
         return (    profile.lender_address,
                     lender.deposited_amount,
                     lender.deposited_amount.sub(lender.locked_amount),
-                    profile.max_risc_score,
+                    profile.max_risk_score,
                     profile.premium_percentage);
     }
+
+    function insureTransfer( address destination ) external payable {
+
+        string memory scoring_system_url = string(abi.encodePacked(
+                "json(https://hackathon-229016.appspot.com/api/?destination_address=0x",
+                toAsciiString(address(destination)), ").score"));
+
+        bytes32 query_id = oraclize_query("URL", scoring_system_url);
+
+        quotes[query_id] = Insurance(msg.sender, destination, msg.value, 0, 0, 0);
+
+        emit NewQuote( msg.sender, destination, msg.value);
+    }
+
+    function __callback(bytes32 myid, string result) public {
+       if (msg.sender != oraclize_cbAddress()) revert();
+
+       Insurance storage insurance = quotes[myid];
+       require(insurance.sender!=address(0));
+
+       uint risk_score = parseInt(result);
+
+       // TODO: find appropriate lender
+       // TODO: calculate premium
+       // TODO: send funds
+
+       emit DoneQuote(insurance.sender, insurance.destination, insurance.amount, risk_score);
+    }
+
+    // TODO: dispute resolution
+
+    function toAsciiString(address x) pure private returns (string memory) {
+        bytes memory s = new bytes(40);
+        for (uint i = 0; i < 20; i++) {
+            byte b = byte(uint8(uint(x) / (2**(8*(19 - i)))));
+            byte hi = byte(uint8(b) / 16);
+            byte lo = byte(uint8(b) - 16 * uint8(hi));
+            s[2*i] = char(hi);
+            s[2*i+1] = char(lo);
+        }
+        return string(s);
+    }
+
+    function char(byte b) pure private returns (byte c) {
+        if (b < 10) return byte(uint8(b) + 0x30);
+        else return byte(uint8(b) + 0x57);
+    }
+
 
 }
